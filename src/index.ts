6ea070331e4be905dcd20b40d785f94408e217cf
FranklyNathan <spamemailldsaopqasdfasd@gmail.com>
2025-12-02 20:55:21 -0800

    v6.8

--- src/index.ts
+++ src/index.ts
@@ -48,6 +48,7 @@ import v6_4 from '../assets/PatchNotes/v6.4 Patch Notes.txt';
 import v6_5 from '../assets/PatchNotes/v6.5 Patch Notes.txt';
 import v6_6 from '../assets/PatchNotes/v6.6 Patch Notes.txt';
 import v6_7 from '../assets/PatchNotes/v6.7 Patch Notes.txt';
+import v6_8 from '../assets/PatchNotes/v6.8 Patch Notes.txt';
 import patchNotesHtml from './html.patchNotes.html';
 import { closeOrErrorHandler, handleClientMessage } from './mod.clientCommunication';
 import gymsText from '../assets/gyms.txt';
@@ -362,6 +363,7 @@ export default {
           'v6.5 Patch Notes.txt': v6_5,
           'v6.6 Patch Notes.txt': v6_6,
           'v6.7 Patch Notes.txt': v6_7,
+          'v6.8 Patch Notes.txt': v6_8,
         };
         const content = map[filename];
         if (content !== undefined) {
